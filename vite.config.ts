940f5153277ff6704092c069ad26c4374d198799
Tor Arne Sandstrak <github@logi.pm>
2025-02-28 21:56:25 +0100

    Fixes #69;
    Had to increase workbox.maximumFileSizeToCacheInBytes due to the hashmap going above default 2MB cache size.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     VitePWA({
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3145728, // 3MB
+      },
       registerType: "autoUpdate",
     }),
   ],
