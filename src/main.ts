8629c634f18c6e775e2c613d34ff86cb7cc0b3bb
R5dan <r5dan@outlook.com>
2025-03-07 17:57:58 +0000

    rebrand to quick duck

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,19 @@ function noSearchDefaultPageRender() {
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
-        <h1>Und*ck</h1>
+        <h1>Quick Duck</h1>
         <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
+        <br />
+        <hr />
+        <br />
+        <p>This is a fork of <a href="https://github.com/t3dotgg/unduck" target="_blank">Unduck</a> </p>
+        <p>If you have any issues, please <a href="https://github.com/R5dan/unduck/discussions" target="_blank">report them here</a></p>
+
         <div class="url-container"> 
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://quickduck.vercel.app?q=%s"
             readonly 
           />
           <button class="copy-button">
@@ -21,11 +27,9 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
+        <a href="https://unduck.link" target="_blank">unduck</a>
         •
-        <a href="https://x.com/theo" target="_blank">theo</a>
-        •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/r5dan/quickduck" target="_blank">github</a>
       </footer>
     </div>
   `;
